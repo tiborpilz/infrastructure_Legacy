d0688ddf3c50dc853ae9b8fca67a6b6e1f39b4c2
Tibor Pilz <tibor.pilz@avenga.com>
2021-07-27 08:09:18 +0200

    move domain to tfvars

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ variable "cloudflare_email" {}
 variable "cloudflare_api_token" {}
 variable "nodecount" {}
 variable "metallb_secret" {}
+variable "domain" {}
 
 terraform {
   required_providers {
@@ -22,7 +23,6 @@ terraform {
 }
 
 locals {
-  domain = "tiborpilz.dev"
   names = [for i in range(var.nodecount) : format("%s%02d", "node", i)]
 }
 
@@ -41,7 +41,7 @@ provider "rke" {
 
 data "cloudflare_zones" "zone" {
   filter {
-    name = local.domain
+    name = var.domain
   }
 }
 
@@ -110,14 +110,14 @@ resource "hcloud_floating_ip" "floating_ip" {
 resource "cloudflare_record" "nodes" {
   for_each = hcloud_server.nodes
   zone_id = lookup(data.cloudflare_zones.zone.zones[0], "id")
-  name    = "${each.value.name}.kube.${local.domain}"
+  name    = "${each.value.name}.kube.${var.domain}"
   type    = "A"
   value   = each.value.ipv4_address
 }
 
 resource "cloudflare_record" "ingress" {
   zone_id = lookup(data.cloudflare_zones.zone.zones[0], "id")
-  name = "*.${local.domain}"
+  name = "*.${var.domain}"
   type = "A"
   value = hcloud_floating_ip.floating_ip.ip_address
 }
