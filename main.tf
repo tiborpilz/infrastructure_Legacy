54918e580d7b52f370766783840e99cdf497c837
Tibor Pilz <tibor.pilz@avenga.com>
2021-08-30 16:03:00 +0200

    Formatting

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ provider "hcloud" {
 }
 
 provider "cloudflare" {
-  email = var.cloudflare_email
+  email     = var.cloudflare_email
   api_token = var.cloudflare_api_token
 }
 
@@ -53,31 +53,31 @@ resource "tls_private_key" "ssh_key" {
 }
 
 resource "hcloud_ssh_key" "terraform" {
-	name = "Terraform ssh key"
-	public_key = tls_private_key.ssh_key.public_key_openssh
+  name       = "Terraform ssh key"
+  public_key = tls_private_key.ssh_key.public_key_openssh
 }
 
 resource "hcloud_server" "nodes" {
-  for_each = toset(local.names)
-  name = each.value
-  image = "ubuntu-18.04"
+  for_each    = toset(local.names)
+  name        = each.value
+  image       = "ubuntu-18.04"
   server_type = "cx21"
-  ssh_keys = [hcloud_ssh_key.terraform.id]
-	user_data = file("userdata.cloudinit")
-  location = "nbg1"
-  keep_disk = true
+  ssh_keys    = [hcloud_ssh_key.terraform.id]
+  user_data   = file("userdata.cloudinit")
+  location    = "nbg1"
+  keep_disk   = true
 
   labels = {
-    type = "kube-node"
+    type      = "kube-node"
     terraform = "true"
   }
 
   provisioner "remote-exec" {
     connection {
-      type = "ssh"
-      user = "root"
+      type        = "ssh"
+      user        = "root"
       private_key = tls_private_key.ssh_key.private_key_pem
-      host = self.ipv4_address
+      host        = self.ipv4_address
     }
     inline = [
       "cloud-init status --wait"
@@ -86,15 +86,15 @@ resource "hcloud_server" "nodes" {
 }
 
 resource "hcloud_floating_ip" "floating_ip" {
-  type = "ipv4"
+  type      = "ipv4"
   server_id = hcloud_server.nodes[local.names[0]].id
 
   provisioner "remote-exec" {
     connection {
-      type = "ssh"
-      user = "root"
+      type        = "ssh"
+      user        = "root"
       private_key = tls_private_key.ssh_key.private_key_pem
-      host = hcloud_server.nodes[local.names[0]].ipv4_address
+      host        = hcloud_server.nodes[local.names[0]].ipv4_address
     }
     inline = [
       "sudo ip addr add ${self.ip_address} dev eth0"
@@ -104,20 +104,20 @@ resource "hcloud_floating_ip" "floating_ip" {
 
 resource "cloudflare_record" "nodes" {
   for_each = hcloud_server.nodes
-  zone_id = lookup(data.cloudflare_zones.zone.zones[0], "id")
-  name    = "${each.value.name}.kube.${var.domain}"
-  type    = "A"
-  value   = each.value.ipv4_address
+  zone_id  = lookup(data.cloudflare_zones.zone.zones[0], "id")
+  name     = "${each.value.name}.kube.${var.domain}"
+  type     = "A"
+  value    = each.value.ipv4_address
 }
 
 resource "cloudflare_record" "ingress" {
   zone_id = lookup(data.cloudflare_zones.zone.zones[0], "id")
-  name = "*.${var.domain}"
-  type = "A"
-  value = hcloud_floating_ip.floating_ip.ip_address
+  name    = "*.${var.domain}"
+  type    = "A"
+  value   = hcloud_floating_ip.floating_ip.ip_address
 }
 
 resource "local_file" "ssh_key" {
   filename = "${path.root}/sshkey"
-  content = tls_private_key.ssh_key.private_key_pem
+  content  = tls_private_key.ssh_key.private_key_pem
 }
