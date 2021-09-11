ba292f7f2252ad436cefb96d074d7a4fe5bebec3
Tibor Pilz <tibor@pilz.berlin>
2021-09-11 18:33:58 +0200

    add app of apps

---
+++ applications/app-of-apps.tf
@@ -0,0 +1,40 @@
+resource "kubernetes_manifest" "app-of-apps-application" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind = "Application"
+    metadata = {
+      finalizers = [
+        "resources-finalizer.argocd.argoproj.io",
+      ]
+      name = "app-of-apps"
+      namespace = "argocd"
+    }
+    spec = {
+      destination = {
+        name = "in-cluster"
+        namespace = "applications"
+        server = ""
+      }
+      project = "default"
+      source = {
+        path = "applications/manifests"
+        repoURL = "git@github.com:tiborpilz/infrastructure.git"
+        targetRevision = "HEAD"
+      }
+      syncPolicy = {
+        automated = {
+          prune = true
+          selfHeal = true
+        }
+        syncOptions = [
+          "CreateNamespace=true",
+        ]
+      }
+    }
+  }
+  wait_for = {
+    fields = {
+      "status.health.status" = "Healthy"
+    }
+  }
+}
