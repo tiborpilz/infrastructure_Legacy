c511bb2edbc66275d0d9474be9ad255d6da01137
Tibor Pilz <tibor@pilz.berlin>
2023-12-28 23:26:41 +0100

    chore: add terraform-docs pre-commit hook

--- terragrunt/scripts/create_docs.sh
+++ terragrunt/scripts/create_docs.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 # This script creates the documentation for all the terraform modules using terraform-docs.
 
+# Abort if terraform-docs is not installed
+if ! command -v terraform-docs &> /dev/null
+then
+    echo "terraform-docs could not be found"
+    exit
+fi
+
+echo "Creating documentation for all modules..."
+
 basedir=$(dirname $0)
 modules=$(find . -name "main.tf" -exec dirname {} \; | sort -u)
-echo "Found modules: $modules"
 
 for module in $modules; do
   terraform-docs markdown $module > $module/README.md
