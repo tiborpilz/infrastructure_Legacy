72a837bf3d0461a84ef2f8c6d9b7f317f2de3061
Tibor Pilz <tibor@pilz.berlin>
2023-12-29 11:59:24 +0100

    chore: improve docs script for usage in git hooks

--- terragrunt/scripts/create_docs.sh
+++ terragrunt/scripts/create_docs.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 # This script creates the documentation for all the terraform modules using terraform-docs.
 
+function echo_if_tty() {
+  if [[ $- == *i* ]]; then echo $1; fi
+}
+
 # Abort if terraform-docs is not installed
 if ! command -v terraform-docs &> /dev/null
 then
-    echo "terraform-docs could not be found"
-    exit
+  echo_if_tty "terraform-docs could not be found"
+  exit
 fi
 
-echo "Creating documentation for all modules..."
+echo_if_tty "Creating documentation for all modules..."
 
 basedir=$(dirname $0)
 modules=$(find . -name "main.tf" -exec dirname {} \; | sort -u)
 
 for module in $modules; do
   terraform-docs markdown $module > $module/README.md
+  echo $module/README.md
 done
